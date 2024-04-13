e799ebd2b7943168881356b98b654c4255b39867
sara.moamen <115166767+Sara-72@users.noreply.github.com>
2024-04-13 15:24:16 +0200

    Add files via upload

---
+++ where-to-fly.php
@@ -0,0 +1,530 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wing Wing | Where to fly</title>
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+    
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/where-to-fly.css">
+
+    <style>
+        body{
+            overflow-x: hidden;
+        }
+    </style>
+</head>
+<body>
+
+
+
+    <header>
+    <div id="logo">
+        <img src="images/Wing_Wing-removebg-preview.png">
+
+    </div>
+    <div id="nav">
+        <ul>
+            <li><a href="index (1).html"><span class="material-symbols-outlined" id="home">home</span>Home</a></li>
+            <li><a href="about-us.html"><span class="material-symbols-outlined">draw</span>About us</a></li>
+            <li><a href="where-to-fly.php"><span class="material-symbols-outlined">travel</span>Where to fly</a></li>
+            <li><a href="contact.html"><span class="material-symbols-outlined">call</span>Contact us</a></li>
+        </ul>
+    </div>
+    <a id="sign" href="logout.php">Log out</a>
+</header>
+ <main>
+<section class="home" >
+
+    <div class="image">
+        <img src="images/travel2.png">
+
+    </div>
+
+    <div class="content">
+        <h3><span>Adventure &nbsp;</span> is worthwhile</h3>
+        <p>Here every click could lead to a new adventure. 
+            A gateway to unforgettable experiences.
+            With us you will turn your travel dreams into reality.
+            Because we know that in the end, 
+            it's not the destinations that matter most,
+            but the adventures along the way.</p>
+
+            <a href="#" class="btn">Explore Now !</a>
+
+
+    </div>
+</section>
+
+
+
+<section class="form-container">
+    <form action="">
+
+        <div class="inputbox">
+            <span>Where to</span>
+            <input type="text" placeholder="search places">
+        </div>
+
+
+        <div class="inputbox">
+            <span>arrivals</span>
+            <input type="date" placeholder="search places">
+        </div>
+
+        <div class="inputbox">
+            <span>leaving</span>
+            <input type="date" placeholder="search places">
+        </div>
+
+        <input type="submit" value="book now" class="btn">
+
+
+    </form>
+</section>
+
+
+
+<section class="packages" id="packages">
+    <h1 class="heading">Our <span>packages</span></h1>
+
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bombai.jpg">
+                <h3><i class="fas fa-map-marker-alt"></i>Mumbai</h3>
+            </div>
+            <div class="content">
+                <div class="price">220.99$&nbsp;<span>400$</span></div>
+                <p>Mumbai the city is an experience everyone should endure. 
+                     What you want to do during your travel, you can do all
+                      at one place and that is Mumbai. There are peaceful
+                       moments to be found here.</p>
+                    <a href="" class="btn">book now</a>
+            </div>
+        </div>
+    
+
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/sydney.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Sydney</h3>
+        </div>
+        <div class="content">
+            <div class="price">150.99$&nbsp;<span>200.99$</span></div>
+            <p>A bright, modern city embedded in nature, where gallery-hopping,
+                 surfing, and fine-dining can all take place in a single day.
+                  Sydney offers an urban mix of rich history and contemporary buzz.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/paris.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Paris</h3>
+        </div>
+        <div class="content">
+            <div class="price">300.50$&nbsp;<span>500.99$</span></div>
+            <p>Paris lives up to its hype: A city with unbelievable food
+                 and culture, plus stunning views everywhere you turn.
+                  With 18 arrondissements, it’s a lot to see in one trip,
+                   but each neighborhood has a personality all its own</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/tokyo.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Tokyo</h3>
+        </div>
+        <div class="content">
+            <div class="price">400.99$&nbsp;<span>600.99$</span></div>
+            <p>With its futuristic skyscrapers, unrivaled food scene,
+                 and wild nightlife, Tokyo is a rush of pure adrenaline.
+                  The city is famously cutting-edge, its vintage teahouses 
+                  and peaceful gardens offer a serene escape.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+  
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/berlin.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Berlin</h3>
+        </div>
+        <div class="content">
+            <div class="price">290.99$&nbsp;<span>350.99$</span></div>
+            <p>The greatest
+                 cultural extravaganza that one could imagine.
+                 Berlin combines the culture of New York,
+                  the traffic system of Tokyo, the nature of Seattle,
+                   and the historical treasures of Berlin. You should
+                   really visit Berlin.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="images/turkey.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>istanbul</h3>
+        </div>
+        <div class="content">
+            <div class="price">200.99$&nbsp;<span>210.99$</span></div>
+            <p>Istanbul, a universal beauty where poet and archaeologist,
+                 diplomat and merchant, princess and sailor, northerner and
+                  westerner screams with same admiration.
+                  The city of minarets and markets.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+
+
+        
+
+
+
+</div>
+
+</section>
+
+
+
+
+<section class="trips" id="trips">
+    <h1 class="heading">Our <span>Trips</span></h1>
+
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/moscow.jpg">
+                <h3><i class="fas fa-map-marker-alt"></i>Moscow</h3>
+            </div>
+            <div class="content">
+                <div class="price">220.99$&nbsp;</div>
+                <p>There are many wonders to be seen in Moscow, the capital of Russia. 
+                    The rich history of the city combines beautiful architecture with
+                     colourful museums.Moscow is a city of contrasts, where beauty and decay coexist in perfect harmony. </p>
+                    <a href="" class="btn">book now</a>
+            </div>
+        </div>
+    
+
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/rome.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Rome</h3>
+        </div>
+        <div class="content">
+            <div class="price">150.99$&nbsp;</div>
+            <p>Rome is home to the famous Colosseum and Trevi Fountain,
+                 the Italian capital is just simply stunning and it’s 
+                 definitely not to be missed when planning a trip to Italy. Once you visit
+                Rome you wll have eternal love for Rome</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/dubai.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Dubai</h3>
+        </div>
+        <div class="content">
+            <div class="price">300.50$&nbsp;</div>
+            <p>Sky's the limit in the city of gold.
+                If you want to sip on luxury at every corner then Dubai should be your next visit.
+                Where every day feels like a scene from a movie.
+                From skyline to shoreline, Dubai has it all.
+                Luxury found its home, and it’s Dubai 
+            </p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/tokyo.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Tokyo</h3>
+        </div>
+        <div class="content">
+            <div class="price">400.99$&nbsp;</div>
+            <p>With its futuristic skyscrapers, unrivaled food scene,
+                 and wild nightlife, Tokyo is a rush of pure adrenaline.
+                  The city is famously cutting-edge, its vintage teahouses 
+                  and peaceful gardens offer a serene escape.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/seol.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Seoul</h3>
+        </div>
+        <div class="content">
+            <div class="price">260.99$&nbsp;</div>
+            <p>Seoul is the South Korean capital and a tourist-loved
+                 destination in Asia. It attracts millions of visitors
+                  each year and charms them with historical sights,
+                 modern skyscrapers, local food traditions, and
+                  unique culture.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+
+    <div class="box">
+        <div class="image">
+            <img src="images/berlin.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>Berlin</h3>
+        </div>
+        <div class="content">
+            <div class="price">290.99$&nbsp;</div>
+            <p>The greatest cultural extravaganza that one could imagine.
+                 Berlin combines the culture of New York, the traffic system of Tokyo,
+                  the nature of Seattle, and the historical treasures of Berlin.
+                   You should really visit Berlin.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="images/turkey.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>istanbul</h3>
+        </div>
+        <div class="content">
+            <div class="price">200.99$&nbsp;</div>
+            <p>Istanbul, a universal beauty where poet and archaeologist,
+                 diplomat and merchant, princess and sailor, northerner and
+                  westerner screams with same admiration. The city of minarets
+                   and markets.</p>
+                <a href="" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="images/swiss.jpg">
+            <h3><i class="fas fa-map-marker-alt"></i>zurich</h3>
+        </div>
+        <div class="content">
+            <div class="price">210.99$&nbsp;</div>
+            <p>Zurich is filled with stunning backdrops that make for the perfect photo.
+            You go to a place for a visit and deep in your heart you think that this place
+             can be your own home and Zurich is such a place!
+            </p>
+                <a href="" class="btn">book now</a>
+        </div>
+ 
+        <?php
+        include("database.php");
+        $query = "SELECT * FROM `trips`";
+        $sql = mysqli_query($conn,$query);
+        if(mysqli_num_rows($sql)>0)
+        {
+
+        while($row=mysqli_fetch_array($sql))
+        {
+
+        
+
+        ?>
+
+        <div class="box">
+                <div class="image">
+                    <img src="images/<?php echo $row['trip_pic']; ?>">
+                    <h3><i class="fas fa-map-marker-alt"></i><?php echo $row['trip_to'] ?></h3>
+                </div>
+                <div class="content">
+                <div class="price">210.99$&nbsp;</div>
+                    <p>Ottawwa is the city of your dreams,
+                        it is a must visit for all people to see it's wonderful
+                        places and experience an adrenaline rush
+                    </p>
+                        <a href="" class="btn">book now</a>
+                </div>
+
+                <?php
+        }
+    }
+
+                ?>
+
+
+       
+    </div>
+    
+
+
+
+
+
+        
+
+
+
+</div>
+
+</section>
+
+
+
+
+<section class="pricing" id="pricing">
+
+
+    <h1 class="heading">Our <span>pricing</span></h1>
+    <div class="box-container">
+
+        <div class="box">
+            <h3>basic plan</h3>
+            <div class="price">
+                <span>$</span>
+                <span class="amount">30</span>
+                <span>/mo</span>
+            </div>
+            <ul>
+                <li>10 days</li>
+                <li>food & drinks</li>
+                <li>safety guide</li>
+                <li>insurance</li>
+
+            </ul>
+            <a href="#" class="btn">choose plan</a>
+
+        </div>
+
+        
+
+
+    
+
+        <div class="box">
+            <h3>standard plan</h3>
+            <div class="price">
+                <span>$</span>
+                <span class="amount">50</span>
+                <span>/mo</span>
+            </div>
+            <ul>
+                <li>20 days</li>
+                <li>food & drinks</li>
+                <li>safety guide</li>
+                <li>insurance</li>
+
+            </ul>
+            <a href="#" class="btn">choose plan</a>
+
+        </div>
+
+
+        <div class="box">
+            <h3>premium plan</h3>
+            <div class="price">
+                <span>$</span>
+                <span class="amount">90</span>
+                <span>/mo</span>
+            </div>
+            <ul>
+                <li>30 days</li>
+                <li>food & drinks</li>
+                <li>safety guide</li>
+                <li>insurance</li>
+
+            </ul>
+            <a href="#" class="btn">choose plan</a>
+
+        </div>
+
+    
+    </div>
+
+
+</section>
+
+
+</main>
+
+
+
+<footer>
+
+    
+    
+    <div id="column">
+    <ul class="social-media">
+        
+        
+                        
+        <li><a href="#"><i class="bx bxl-facebook"></i></a><span>Facebook</span></li>
+        <li><a href="#"><i class="bx bxl-twitter"></i></a><span>Twitter</span></li>
+        <li><a href="#"><i class="bx bxl-instagram"></i></a><span>Instagram</span></li>
+        <li><a href="#"><i class="bx bxl-youtube"></i></a><span>Youtube</span></li>
+    </ul>
+
+
+<div class="footer-bottom">
+
+    <p>&copy;2024 Wing wing All right resreved</p>
+</div>
+
+</div>
+
+
+
+
+</footer>
+
+<script>
+    window.addEventListener('scroll', function() {
+        var header = document.querySelector('header');
+        header.classList.toggle('scrolled', window.scrollY > 0);
+    });
+    
+</script>
+    
+</body>
+</html>
